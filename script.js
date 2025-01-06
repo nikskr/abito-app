7ee582dda617145fb7d88bc3a61a5f250f75dc3d
nikskr <nskrabnevsky@mail.ru>
2025-01-07 02:05:39 +0300

    init project

---
+++ script.js
@@ -0,0 +1,93 @@
+
+const searchBtn = document.querySelector('.search-btn');
+const searchInput = document.querySelector('.search-box input');
+const cardWrapper = document.querySelector('.content-main__list');
+
+const cardArray = [
+    {
+        id: 0,
+        image: './images/card-img.png',
+        title: 'Первый товар',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+    },
+    {
+        id: 1,
+        image: './images/card-img.png',
+        title: 'Второй товар',
+        price: '171 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+    },
+    {
+        id: 2,
+        image: './images/card-img.png',
+        title: 'Третий товар',
+        price: '172 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+    },
+    {
+        id: 3,
+        image: './images/card-img.png',
+        title: 'Четвертый товар',
+        price: '173 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+    },
+    {
+        id: 4,
+        image: './images/card-img.png',
+        title: 'Пятый товар',
+        price: '174 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+    },
+    {
+        id: 5,
+        image: './images/card-img.png',
+        title: 'Шестой товар',
+        price: '175 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+    },
+]
+
+const render = (cardList) => {
+    cardWrapper.innerHTML = '';
+
+    cardList.forEach((item, index) => {
+        cardWrapper.insertAdjacentHTML('beforeend', `
+            <a href="./product.html" class="content-main__list-item">
+                <div class="content-main__list-item--img">
+                    <img src=${item.image} alt=${item.title}>
+                </div>
+                <h5 class="content-main__list-item--title">
+                    ${item.title}
+                </h5>
+                <strong class="content-main__list-item--price">${item.price}</strong>
+                <div class="content-main__list-item--desc-box">
+                    <span class="content-main__list-item--desc">${item.address}</span>
+                    <span class="content-main__list-item--desc">${item.date}</span>
+                </div>
+            </a>
+            `)
+    })
+}
+
+const filteredArray = (array, value) => {
+    return array.filter((item) => {
+        return item.title.includes(value) || item.price.includes(value);
+    });
+}
+
+cardWrapper.style.justifyContent = 'flex-start';
+cardWrapper.style.rowGap = 'normal'; 
+cardWrapper.style.gap = '30px'; 
+
+render(cardArray);
+
+searchBtn.addEventListener('click', () => {
+    render(filteredArray(cardArray, searchInput.value));
+})
